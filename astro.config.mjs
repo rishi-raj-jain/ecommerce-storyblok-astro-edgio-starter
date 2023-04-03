5d4e836a639bd3389958a4c748afd7c79950b6f8
Rishi Raj Jain <rjain@llnw.com>
2023-04-03 22:09:01 +0530

    add ssl

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,29 @@
+import * as dotenv from 'dotenv'
 import node from '@astrojs/node'
 import react from '@astrojs/react'
+import storyblok from '@storyblok/astro'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+dotenv.config()
 
 export default defineConfig({
-  integrations: [react(), tailwind()],
   output: 'server',
   adapter: node({
     mode: 'standalone',
   }),
+  integrations: [
+    react(),
+    tailwind(),
+    storyblok({
+      accessToken: process.env.STORYBLOK,
+    }),
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
 })
